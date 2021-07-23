51e0009b39b29c598ec99a042b7d69d2016b4aa7
akiko.nishimoto <work@ainezu.com>
2021-07-23 06:39:06 +0000

    kadai4

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_014455) do
+ActiveRecord::Schema.define(version: 2021_07_23_061315) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..10).each do |number|
+  Task.create(status: 'SUCCESS', content: 'test task ' + number.to_s)
+end
